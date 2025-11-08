6982bd582a6e596058eceb147bb4cdd4aca7140c
Chestnut Sea <103401740+chestnutsea@users.noreply.github.com>
2025-11-08 22:48:18 +0800

    完成了数据基建

---
+++ Persistence/FeatureEntity+CoreDataClass.swift
@@ -0,0 +1,17 @@
+//
+//  FeatureEntity+CoreDataClass.swift
+//  Project Color
+//
+//  Created by Linya Huang on 2025/11/8.
+//
+//
+
+public import Foundation
+public import CoreData
+
+public typealias FeatureEntityCoreDataClassSet = NSSet
+
+@objc(FeatureEntity)
+public class FeatureEntity: NSManagedObject {
+
+}
